82d945e5c75c3cd59f70886a859a8d32c6762387
The Programmer <shrey.b.tank@gmail.com>
2025-03-14 19:09:12 +0530

    fixed file

--- Backtracking/NQueen.cpp
+++ Backtracking/NQueen.cpp
@@ -27,13 +27,13 @@ bool isSafe(vector<vector<int>> &board, int row, int col, int n){
         }
     }
 
-    for(int i=row, j=col; i>=0, j>=0; i--, j--){
+    for(int i=row, j=col; i>=0 && j>=0; i--, j--){
         if (board[i][j] == 1){
             return false;
         }
     }
 
-    for(int i=row, j=col; i>=0, j<n; i--, j++){
+    for(int i=row, j=col; i>=0 && j<n; i--, j++){
         if (board[i][j] == 1){
             return false;
         }
